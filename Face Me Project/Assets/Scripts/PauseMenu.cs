e5dbbc9bf8538c6c7fa128fc31b1c2b5df7629b0
Cory Reardon <cjreardon@gmail.com>
2021-03-13 13:04:11 -0800

    Added the Pause Menu

---
+++ Face Me Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public AudioMixer audioMixer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void LoadMenu() {
+        Debug.Log("Loading menu...");
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame() {
+        Debug.Log("Quiting Game...");
+        Application.Quit();
+    }
+
+    public void SetVolume (float volume) {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
