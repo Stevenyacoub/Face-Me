ffab5949a21709b59979fc312e79297bcc3b5ffe
Stevenyacoub <yacoubsteven@gmail.com>
2021-04-03 14:01:09 -0700

    Gallery Mode Sound + Shatter Animation
    This adds Jinsei's audio track into the Gallery Mode in addition to a new animation where pressing the spacebar will make the segmented head sculpture slide apart.

---
+++ Face Me Project/Assets/Scripts/HeadAction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadAction : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            anim.SetTrigger("FallApart");
+        }
+    }
+}
